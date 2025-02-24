9411ef27f029af02804394dff4e9fea71893d0b8
nyel-101 <dandevjr@gmail.com>
2025-02-24 21:29:36 +0000

    feat(worker): sanitize payload parser

--- fury/src/controllers/constants.ts
+++ fury/src/controllers/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fd44871975404b47843276562cf9c048 = () => {
-  return '2026-02-27T01:59:47Z';
+const node_fe6612b125b049b2915ae8f54831a22f = () => {
+  return '2025-02-24T21:29:36Z';
 };
