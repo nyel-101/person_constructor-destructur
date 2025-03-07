3a3deb3e2460f9b7c75a55591080f7bf52be46d6
nyel-101 <dandevjr@gmail.com>
2025-03-07 23:07:15 +0000

    fix(core): sanitize database index

--- furry/lib/routes.js
+++ furry/lib/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c8da2a0b82124c49b532d88922f7b6a1 = () => {
-  return '2026-06-14T10:28:24Z';
+const node_5c5dc7eaf53347e5a08da3e06a32f16f = () => {
+  return '2025-03-07T23:07:15Z';
 };
