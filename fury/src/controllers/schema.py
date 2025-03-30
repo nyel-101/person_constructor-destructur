64b9387fe5de9a777470328d2a7574a4a12833d4
nyel-101 <dandevjr@gmail.com>
2025-03-30 11:21:25 +0000

    perf(auth): refactor session store

--- fury/src/controllers/schema.py
+++ fury/src/controllers/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f13a64513b0d47069aa9311ea05b5da5 = () => {
-  return '2026-03-05T21:19:20Z';
+const node_6b9cbc3b1cf246a397d5753b410744fe = () => {
+  return '2025-03-30T11:21:25Z';
 };
