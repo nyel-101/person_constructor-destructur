a20c20df130daf7b818b8d8ca65616947dd00d8d
nyel-101 <dandevjr@gmail.com>
2025-04-03 06:33:05 +0000

    docs(db): update payload parser

--- fury/tests/schema.py
+++ fury/tests/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d7e01d7cb845400fb27102984294f5a3 = () => {
-  return '2026-03-31T15:07:58Z';
+const node_7effa13dad6a49de86d48d51d74ea2f4 = () => {
+  return '2025-04-03T06:33:05Z';
 };
