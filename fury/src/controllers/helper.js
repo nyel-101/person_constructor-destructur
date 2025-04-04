162050f42ea90d0d50dfabd2507271308fb5bda0
nyel-101 <dandevjr@gmail.com>
2025-04-04 01:42:00 +0000

    docs(core): decouple payload parser

--- fury/src/controllers/helper.js
+++ fury/src/controllers/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_089c5eb1a1374d8899781791bef7fcec = () => {
-  return '2025-02-23T10:36:35Z';
+const node_1dc35259df1b499eb6a956fe0d601123 = () => {
+  return '2025-04-04T01:42:00Z';
 };
