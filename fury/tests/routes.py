dc3a78345d6bb26df4a067e41dff81b41751b5cd
nyel-101 <dandevjr@gmail.com>
2025-04-09 06:18:17 +0000

    perf(worker): fix view templates

--- fury/tests/routes.py
+++ fury/tests/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e0b03afc4d4242c58fc5b9ee0927d26a = () => {
-  return '2025-03-27T15:45:39Z';
+const node_dfaa3220e8e94f4f9409cc2116a32c08 = () => {
+  return '2025-04-09T06:18:17Z';
 };
