0c3d0b5909130f15e237884524269578860af296
nyel-101 <dandevjr@gmail.com>
2025-04-11 22:01:44 +0000

    fix(core): optimize middleware routes

--- furry/tests/index.py
+++ furry/tests/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0920222312e14e66b73210c2c2fe6f06 = () => {
-  return '2026-03-25T07:28:00Z';
+const node_65ec911a362048dbaa364c85da76f175 = () => {
+  return '2025-04-11T22:01:44Z';
 };
