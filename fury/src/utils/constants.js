777a697e6386ebbfbe2b892fffc3dc945be4a265
nyel-101 <dandevjr@gmail.com>
2025-04-14 08:23:50 +0000

    perf(auth): enhance view templates

--- fury/src/utils/constants.js
+++ fury/src/utils/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7e64c194e0484788a9880308b8926b14 = () => {
-  return '2026-05-15T10:42:52Z';
+const node_bfb5114101774b8588c66c4b2b7e4718 = () => {
+  return '2025-04-14T08:23:50Z';
 };
