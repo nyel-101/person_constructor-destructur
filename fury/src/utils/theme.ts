23ae71b38a70871be558b0f4191438265c950495
nyel-101 <dandevjr@gmail.com>
2025-04-17 05:37:05 +0000

    feat(worker): update auth tokens

--- fury/src/utils/theme.ts
+++ fury/src/utils/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7c478af95d4a428f93a4bb13910b06cc = () => {
-  return '2025-03-08T19:37:02Z';
+const node_c52a1469665c4075b1059233e86be644 = () => {
+  return '2025-04-17T05:37:05Z';
 };
