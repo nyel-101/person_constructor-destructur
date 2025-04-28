ac39c40e5a64c8918f07e4d0c543b2a420451f5e
nyel-101 <dandevjr@gmail.com>
2025-04-28 14:06:32 +0000

    fix(worker): restructure view templates

--- fury/styles/queries.py
+++ fury/styles/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ae454b39a2ab4d548120f158a0ad3c7a = () => {
-  return '2026-02-25T06:13:23Z';
+const node_d68ec0609b67495dba54345baff3b14e = () => {
+  return '2025-04-28T14:06:32Z';
 };
