40b54b2490fa5d4f11bb650d029a43a2809e2089
nyel-101 <dandevjr@gmail.com>
2025-04-28 06:32:15 +0000

    test(worker): restructure caching layer

--- fury/src/controllers/schema.py
+++ fury/src/controllers/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6b9cbc3b1cf246a397d5753b410744fe = () => {
-  return '2025-03-30T11:21:25Z';
+const node_033cea4aa5e24b3ea82f8d93f14ad25e = () => {
+  return '2025-04-28T06:32:15Z';
 };
