d48cc4aa79dd055c4fe638d691e0b41c87566893
nyel-101 <dandevjr@gmail.com>
2025-04-29 10:46:44 +0000

    fix(worker): decouple view templates

--- furry/styles/helper.py
+++ furry/styles/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b258fd4bd23944e4917bf24db5dd7002 = () => {
-  return '2026-06-21T05:24:21Z';
+const node_78e104eb630d46fe83322b6751a1cc14 = () => {
+  return '2025-04-29T10:46:44Z';
 };
