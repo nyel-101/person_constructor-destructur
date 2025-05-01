cfb40319e80f54147ebb591731a7dbccb618a329
nyel-101 <dandevjr@gmail.com>
2025-05-01 12:56:56 +0000

    perf(worker): enhance middleware routes

--- fury/src/controllers/helper.ts
+++ fury/src/controllers/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_326eb51d45624bd499c0f02cb0449580 = () => {
-  return '2025-04-03T15:13:19Z';
+const node_bba92c05159e4fe5b89c762f943510e0 = () => {
+  return '2025-05-01T12:56:56Z';
 };
