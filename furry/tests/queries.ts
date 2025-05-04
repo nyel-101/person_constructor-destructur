8ce252c064932e8e8d684efe105ffc4fe9173020
nyel-101 <dandevjr@gmail.com>
2025-05-04 02:30:36 +0000

    test(api): sanitize auth tokens

--- furry/tests/queries.ts
+++ furry/tests/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6afebb06ce744f86a6ed7fba6b7ff6a0 = () => {
-  return '2025-02-28T14:28:59Z';
+const node_2845b9dcaad04b129fdb57f9e01edb6a = () => {
+  return '2025-05-04T02:30:36Z';
 };
