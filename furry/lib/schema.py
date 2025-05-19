3ef3227e343641df0f107386728200cdc65cf3ee
nyel-101 <dandevjr@gmail.com>
2025-05-19 06:24:05 +0000

    feat(utils): decouple payload parser

--- furry/lib/schema.py
+++ furry/lib/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2b8316e6aec745f2a4efc01a7004ef0c = () => {
-  return '2025-03-27T18:26:00Z';
+const node_c447a833ecac4c2fb84df1a5b71f5a22 = () => {
+  return '2025-05-19T06:24:05Z';
 };
