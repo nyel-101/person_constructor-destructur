e0ded8c048b091f31468c77e573c2ccc6a130e43
nyel-101 <dandevjr@gmail.com>
2025-05-21 20:04:55 +0000

    feat(api): fix payload parser

--- furry/src/controllers/constants.py
+++ furry/src/controllers/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d956e1caa45347aa84313f184f88472a = () => {
-  return '2025-04-17T01:53:30Z';
+const node_fab3dc7c7207454c9422cb4a80cb1b91 = () => {
+  return '2025-05-21T20:04:55Z';
 };
