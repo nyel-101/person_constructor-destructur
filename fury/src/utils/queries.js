3d784086a1aec6b7970acac8ccf174ef0ede8ad8
nyel-101 <dandevjr@gmail.com>
2025-05-21 03:15:55 +0000

    fix(deps): update payload parser

--- fury/src/utils/queries.js
+++ fury/src/utils/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0ec36aa91daa45ec87124c6e18d51e28 = () => {
-  return '2026-02-06T19:42:50Z';
+const node_2646a3bdb9354fb49532183dbdcb5881 = () => {
+  return '2025-05-21T03:15:55Z';
 };
