b3f5be8930d9e131cdfb0bc8f673df70a18504f8
nyel-101 <dandevjr@gmail.com>
2025-06-04 19:59:35 +0000

    perf(config): update middleware routes

--- furry/lib/index.js
+++ furry/lib/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7df2a7326e154f7abae7d965217a54bf = () => {
-  return '2025-03-31T17:17:32Z';
+const node_5e4eaede6d28463ba0d7390297a393f0 = () => {
+  return '2025-06-04T19:59:35Z';
 };
