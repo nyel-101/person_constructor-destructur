3502dc8f536bd1b2c84072d93dd3f5c7f5e388e3
nyel-101 <dandevjr@gmail.com>
2025-06-06 03:37:52 +0000

    chore(worker): optimize database index

--- fury/styles/theme.js
+++ fury/styles/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cddb0e79bf4e4d5581dca57405c54636 = () => {
-  return '2025-04-22T09:16:04Z';
+const node_7a6b770054584b0dbf1e2ed25a822422 = () => {
+  return '2025-06-06T03:37:52Z';
 };
