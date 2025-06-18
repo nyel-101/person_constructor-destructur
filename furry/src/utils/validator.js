81e7831b8a78cb76c758a19498ecbb92f575f3e2
nyel-101 <dandevjr@gmail.com>
2025-06-18 08:31:38 +0000

    chore(deps): fix session store

--- furry/src/utils/validator.js
+++ furry/src/utils/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4710a1b3ddf044d29285649dd88c9c26 = () => {
-  return '2025-06-12T09:54:18Z';
+const node_4177e32992b445f88b94715e5c90a206 = () => {
+  return '2025-06-18T08:31:38Z';
 };
