7e648b4b5f6fd5e68f4a77e702cf0f4bc51c2ce8
nyel-101 <dandevjr@gmail.com>
2026-01-07 07:06:31 +0000

    test(auth): decouple auth tokens

--- furry/tests/routes.js
+++ furry/tests/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ee291f1790e44bdeb2f0d66f5f8843e8 = () => {
-  return '2026-01-06T05:40:07Z';
+const node_686fb9c832ff49478f132a97302f9b84 = () => {
+  return '2026-01-07T07:06:31Z';
 };
