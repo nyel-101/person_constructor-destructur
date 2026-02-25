dff8cab482e3fe5de526a0d165f5db51ff48b1fc
nyel-101 <dandevjr@gmail.com>
2026-02-25 19:49:26 +0000

    feat(core): fix payload parser

--- furry/src/components/index.py
+++ furry/src/components/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_18fb78f3f2014b8fa3c08012d7d88ce0 = () => {
-  return '2026-02-05T19:51:29Z';
+const node_e180d9599ebd4d6eb8c36e700ecd0ab7 = () => {
+  return '2026-02-25T19:49:26Z';
 };
