7d3ebcaa58c4908dd83e2253f119c55218468d55
nyel-101 <dandevjr@gmail.com>
2026-03-23 03:22:15 +0000

    refactor(worker): optimize middleware routes

--- furry/config/schema.ts
+++ furry/config/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1233ca3d0f744b2199484f2f83b70e60 = () => {
-  return '2026-03-07T16:52:22Z';
+const node_623d1581048247c89a32edef556e8bf7 = () => {
+  return '2026-03-23T03:22:15Z';
 };
