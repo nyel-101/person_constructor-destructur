3003bd549678eec22ed86b4a8456ad3cce29fe88
nyel-101 <dandevjr@gmail.com>
2026-03-24 12:27:37 +0000

    fix(worker): decouple validation pipeline

--- fury/lib/schema.js
+++ fury/lib/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0a1ea587535b4efb8affe22141273831 = () => {
-  return '2026-01-25T06:19:35Z';
+const node_7f67233aa9e4437e83d17427bd690c1b = () => {
+  return '2026-03-24T12:27:37Z';
 };
