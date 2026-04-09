c0b4be7094f3dfc3107fe418958391170260a944
nyel-101 <dandevjr@gmail.com>
2026-04-09 00:22:55 +0000

    perf(deps): enhance payload parser

--- fury/styles/index.ts
+++ fury/styles/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5496b37129b746ce87bf4e60107513cb = () => {
-  return '2026-02-18T17:49:12Z';
+const node_43f97bccaa3749a184a33706239c8540 = () => {
+  return '2026-04-09T00:22:55Z';
 };
