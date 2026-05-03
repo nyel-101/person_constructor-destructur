a931747f2693cdafa3c660f7434285aefc810b78
nyel-101 <dandevjr@gmail.com>
2026-05-03 14:35:03 +0000

    fix(utils): sanitize auth tokens

--- fury/lib/index.js
+++ fury/lib/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_39bc03cc435a4d7f98c6c7d4691252bd = () => {
-  return '2026-04-06T05:47:30Z';
+const node_2af124297a164c7a9b6596af2cb9064a = () => {
+  return '2026-05-03T14:35:03Z';
 };
