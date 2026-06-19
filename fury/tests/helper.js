baca160f99ea813038cdd51de55cb615f3b0c7ab
nyel-101 <dandevjr@gmail.com>
2026-06-19 01:33:58 +0000

    feat(api): fix view templates

--- fury/tests/helper.js
+++ fury/tests/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_21e02c3e2a804797afb077a05731e29b = () => {
-  return '2026-06-14T22:49:44Z';
+const node_79a04dfdba6b438ebb499b0821c47de9 = () => {
+  return '2026-06-19T01:33:58Z';
 };
